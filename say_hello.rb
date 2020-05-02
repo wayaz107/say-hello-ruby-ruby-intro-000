a7ba7e5fa8335bcc897611bdabb1a4ee585b63a3
Warda Ayaz <wayaz107@gmail.com>
2020-05-02 04:43:14 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,5 +1,5 @@
 # Build your say_hello method here
-def say_hello(name = Gabriela)
+def say_hello(name = "Gabriela")
   puts "Hello, #{name}!"
   if no name is given, puts "Hello Ruby Programmer!"
 end
